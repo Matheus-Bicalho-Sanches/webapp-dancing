c4d6b84fd4036dbcd2c745ad747f813e1cc97f25
Matheus-Bicalho-Sanches <matheussancheslondrina@gmail.com>
2025-03-06 09:20:26 -0300

    Adição de calendário (icone) na seleção de datas da tabela de CRM

--- src/pages/private/CRM.jsx
+++ src/pages/private/CRM.jsx
@@ -1602,7 +1602,21 @@ export default function CRM() {
                               autoFocus
                               size="small"
                               InputProps={{
-                                style: { fontSize: '0.875rem' }
+                                style: { fontSize: '0.875rem' },
+                                endAdornment: (
+                                  <InputAdornment position="end">
+                                    <IconButton
+                                      size="small"
+                                      edge="end"
+                                      onClick={(e) => e.stopPropagation()}
+                                      sx={{ padding: 0 }}
+                                    >
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                                        <path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/>
+                                      </svg>
+                                    </IconButton>
+                                  </InputAdornment>
+                                )
                               }}
                             />
                           ) : (
@@ -1631,7 +1645,21 @@ export default function CRM() {
                               autoFocus
                               size="small"
                               InputProps={{
-                                style: { fontSize: '0.875rem' }
+                                style: { fontSize: '0.875rem' },
+                                endAdornment: (
+                                  <InputAdornment position="end">
+                                    <IconButton
+                                      size="small"
+                                      edge="end"
+                                      onClick={(e) => e.stopPropagation()}
+                                      sx={{ padding: 0 }}
+                                    >
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                                        <path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/>
+                                      </svg>
+                                    </IconButton>
+                                  </InputAdornment>
+                                )
                               }}
                             />
                           ) : (
@@ -1659,7 +1687,21 @@ export default function CRM() {
                               autoFocus
                               size="small"
                               InputProps={{
-                                style: { fontSize: '0.875rem' }
+                                style: { fontSize: '0.875rem' },
+                                endAdornment: (
+                                  <InputAdornment position="end">
+                                    <IconButton
+                                      size="small"
+                                      edge="end"
+                                      onClick={(e) => e.stopPropagation()}
+                                      sx={{ padding: 0 }}
+                                    >
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                                        <path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/>
+                                      </svg>
+                                    </IconButton>
+                                  </InputAdornment>
+                                )
                               }}
                             />
                           ) : (
